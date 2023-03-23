ac8de7869a09fffe8bee414bbbbc133c01699b75
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-03-23 08:31:06 +0100

    Add calculator components

---
+++ src/components/calcDisplay.js
@@ -0,0 +1,10 @@
+function CalcDisplay() {
+  return (
+    <div className="calculatorDisplay">
+      <span className="previous">0</span>
+      <span className="current">0</span>
+    </div>
+  );
+}
+
+export default CalcDisplay;
