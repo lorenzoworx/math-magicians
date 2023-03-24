2d499fab56a85efd06343411d743b6485c2fc74b
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-03-24 08:07:04 +0100

    Implement calculator functionality

--- src/components/calcDisplay.js
+++ src/components/calcDisplay.js
@@ -1,9 +1,15 @@
-function CalcDisplay() {
+import PropTypes from 'prop-types';
+
+function CalcDisplay({ result }) {
   return (
     <div className="calculatorDisplay">
-      <span className="current">0</span>
+      <span className="current">{result}</span>
     </div>
   );
 }
+
+CalcDisplay.propTypes = {
+  result: PropTypes.string.isRequired,
+};
 
 export default CalcDisplay;
