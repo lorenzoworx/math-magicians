ed5cdb65d1913cc84936b263588d092ea352439e
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-03-24 15:04:13 +0100

    Add quote component to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Calculator from './components/calculator';
+import Quote from './components/calcQuote';
 
 function App() {
   return (
     <div className="App">
+      <Quote />
       <Calculator />
     </div>
   );
