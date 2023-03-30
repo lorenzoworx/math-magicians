b936b169dc6124cb1fe3e050cbf65dc5d463590d
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-03-30 14:39:29 +0100

    update nave component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,13 +2,13 @@ import { Outlet } from 'react-router-dom';
 import NavItem from './NavItems';
 
 const NavBar = () => (
-  <nav className="navbar">
-    <div className="nav-container">
+  <>
+    <nav className="navbar">
       <h1>Math Magicians</h1>
       <NavItem />
-    </div>
+    </nav>
     <Outlet />
-  </nav>
+  </>
 );
 
 export default NavBar;
